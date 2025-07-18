1eea8af651c9b818b1a39172bd11114fc27205a8
eureka <tsagocedric@gmail.com>
2025-07-18 06:40:46 +0100

    mise à jour

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob('./pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
