a3a5d404184d47b74c1b85be50fd256a22131809
eureka <tsagocedric@gmail.com>
2025-07-18 06:29:12 +0100

    mise à jour

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue({
@@ -17,10 +16,5 @@ export default defineConfig({
                 },
             },
         }),
-    ],
-    resolve: {
-        alias: {
-        '@': '/resources/js'
-        }
-    }
+    ]
 });
